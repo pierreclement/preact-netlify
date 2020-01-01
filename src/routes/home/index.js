b7d72001e5005d49c85d25b9f4dc0743aaf7f797
Pierre <p1m3nt@gmail.com>
2020-01-01 15:23:53 -0500

    update 1

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -33,7 +33,7 @@ const Home = () => {
 			</div>
 			<div class={style.bio}>
 				<p class={style.bioleft}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					Lorem-lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
 					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
 					aliquip ex ea commodo consequat. Duis aute irure dolor in
